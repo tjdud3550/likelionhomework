956fe1e22e1bd242a3781cabd1782381f1d8fd46
tjdud3550 <tjdud3550@likelion.org>
2019-07-08 16:26:43 +0900

    clean push

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jsy\desktop\20190708likelion\session_crud\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
